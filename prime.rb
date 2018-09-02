a532247b39d362dd3bf6277e7b637d4bb88ce136
Shuo Wang <gken90@gmail.com>
2018-09-02 17:31:54 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
 # Add  code here!
+def prime?(num)
+  if (num < 2)
+    return false
+  else
+    (2...num).each do |factor|
+      if (num % factor == 1)
+        return true
+    end
+end
+
+
+
+#A prime number is a whole number greater than 1 whose only factors are 1 and itself.
+
+
+# if num is 10, 10/2 = 5.
